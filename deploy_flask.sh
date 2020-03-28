a45b1d73705fe1a5017013a3289049302a86bd57
nofaryung <46398364+nofaryung@users.noreply.github.com>
2020-03-28 13:37:38 +0300

    Update deploy_flask.sh
    flask server runing in background (issue #30 resolved)
    
    Co-Authored-By: fakhri said <flippy1989@users.noreply.github.com>

--- deploy_flask.sh
+++ deploy_flask.sh
@@ -8,7 +8,9 @@ apt install -y python3-pip
 pip3 install --upgrade pip
 pip3 install Flask 
 
-#go to the directory containing the file we want to run
 cd /vagrant/
 
-FLASK_APP=hello.py flask run --host=0.0.0.0
+FLASK_APP=hello.py
+sudo python3 hello.py >> log.txt 2>&1 &
+
+exit 0
