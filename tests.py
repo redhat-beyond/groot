b2cada7378a93f949ba2966f3a7088a951e3124a
flippy1989 <39088207+flippy1989@users.noreply.github.com>
2020-04-02 14:44:24 +0300

    the CI with the tests.py file (currently it only makes sure we have a correct connection to the local host as would be expected)

---
+++ tests.py
@@ -0,0 +1,17 @@
+import pytest
+import flask
+import os
+import tempfile
+import urllib.request
+
+SITE_LOCATION = "http://localhost:5000"
+def url_ok(url):
+    try:
+        r = urllib.request.urlopen(url).getcode()
+
+    except:
+        r = 404
+
+    return r
+
+print(url_ok(SITE_LOCATION))
