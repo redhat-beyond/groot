821ab80856bdea5427098c3af9230aeeefdb3fd9
Shay Avraham <shay.avraham100@gmail.com>
2020-05-11 23:08:54 +0300

    added tables to database and added validation to register form --wip

--- groot/models.py
+++ groot/models.py
@@ -1,12 +1,91 @@
+from datetime import datetime
 from groot import db
+
+
+policies_users = db.Table('policies_users',
+                       db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+                       db.Column('policy_id', db.Integer, db.ForeignKey('policies.id')))
+
+sensors_policies = db.Table('sensors_policies',
+                       db.Column('policy_id', db.Integer, db.ForeignKey('policies.id')),
+                       db.Column('sensor_id', db.Integer, db.ForeignKey('sensors.id')))
+
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
+    __table_args__ = {'extend_existing': True}
 
-    def __init__(self, username):
-        self.username = username
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False, unique=False)
+    last_name = db.Column(db.String(30), nullable=False, unique=False)
+    nick_name = db.Column(db.String(30), nullable=False, unique=False)
+    hash_pw = db.Column(db.String(200), nullable=False, unique=False)
+    policies_wrote = db.relationship("Policy", backref="owner", lazy=True)
+    comments_wrote = db.relationship("Comment", backref="author", lazy=True)
+    policies_used = db.relationship("Policy", secondary=policies_users, 
+                                       backref=db.backref('policies_used', lazy='dynamic'))
+
 
     def __repr__(self):
-        return '<User: {}>'.format(self.username)
+        return {'email':self.email, 'first_name':self.first_name,'last_name':self.last_name,
+         'nick_name':self.nick_name,'hash_pw':self.hash_pw}
+      
+    
+
+class Policy(db.Model):
+    __tablename__ = 'policies'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    policy_name = db.Column(db.String(20), nullable=False, unique=True)
+    plant_type = db.Column(db.String(30), nullable=False)
+    humidity = db.Column(db.Integer, nullable=False)
+    amount_light = db.Column(db.Integer, nullable=False)
+    irregation_frequency = db.Column(db.Integer, nullable=False)
+    irregation_amount = db.Column(db.Integer, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+    writer = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    policy_users = db.relationship("User", backref="used_by", lazy=True)
+    policy_comments = db.relationship("Comment", backref="wrote_by", lazy=True)
+    policy_sensors = db.relationship("Sensor", secondary=sensors_policies, 
+                                       backref=db.backref('policy_sensors', lazy='dynamic'))
+
+
+    def __repr__(self):
+        return {'id':self.id, 'policy_name':self.policy_name, 'plant_type':self.plant_type,
+                'humidity':self.humidity, 'amount_light':self.amount_light,
+                 'irregation_frequency':self.irregation_frequency,
+                 'irregation_amount':self.irregation_amount, 'date_created':self.date_created} 
+
+
+
+class Sensor(db.Model):
+    __tablename__ = 'sensors'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    type = db.Column(db.String(30), nullable=False, unique=False)
+    active = db.Column(db.Boolean, nullable=False, unique=False)
+
+
+    def __repr__(self):
+        return {'id':self.id, 'name':self.name,'type':self.type, 'active':self.active}
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=False)
+    content = db.Column(db.Text, nullable=False, unique=False)
+    date_posted = db.Column(db.String(30), nullable=False, unique=True, default=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+    writer = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    policy_id = db.Column(db.Integer, db.ForeignKey('policies.id'), nullable=False)
+
+
+
+    def __repr__(self):
+        return {'id':self.id, 'title':self.title,'content':self.content, 'date_posted':self.date_posted}
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response, render_template, request, redirect, url_for, flash
 import groot
-from groot.models import User
+from groot.models import *
 
 
 app = groot.app
@@ -20,7 +20,21 @@ def login():
     if request.method == 'POST':
         try:
             email = request.form.get('email')
-            password = request.form.get('password')
+            hash_pw = request.form.get('password')
+            #hash check
+            
+            emailCounter = User.query.filter_by(email = email).count()
+
+            if emailCounter < 1:
+                return render_template('error.html',message = 'User not exist')
+            else:
+                #check_password_hash(pwhash,password)
+                count = User.query.filter_by(email=email,hash_pw=hash_pw).count()
+                if count > 0:
+                    return render_template('error.html',message = 'Welcome user')
+                else:
+                    return render_template('error.html',message = 'Incorrect password')
+
             return render_template('register.html')  
         except:
             return '<Error: groot_login[POST]>'
@@ -34,10 +48,35 @@ def register():
         try:
             first_name = request.form.get('first_name')
             last_name = request.form.get('last_name')
+            nick_name = request.form.get('nickname')
             email = request.form.get('email')
             password = request.form.get('password')
             password_repeat = request.form.get('password_repeat')
-            user = User(username=first_name)
+            hash_pw = request.form.get('password')
+
+            #is string empty validation
+            if not first_name:
+              return render_template('error.html',message = 'must provide first_name') 
+            elif not last_name:
+              return render_template('error.html',message = 'must provide last_name')
+            elif not nick_name:
+                return render_template('error.html',message = 'must provide nick name')
+            elif not email:
+                return render_template('error.html',message = 'must provide email')
+            elif not password:
+               return render_template('error.html',message ="must provide password")
+            elif not password_repeat:
+                return render_template('error.html',message ="must confirm password")
+            #compare password to confirmation password
+            elif password_repeat != password:
+                return render_template('error.html',message ="passwords don't match")
+
+            emailCounter = User.query.filter_by(email = email).count()
+            if emailCounter > 0:
+                return render_template('error.html',message = "Email already exist -please use another one")
+                
+            #hash function hashed_password = generate_password_hash(password)
+            user = User(email=email,first_name=first_name,last_name=last_name,nick_name=nick_name,hash_pw=hash_pw)
             db.session.add(user)
             db.session.commit()
             return render_template('login.html')
@@ -45,6 +84,10 @@ def register():
             return '<Error: groot_register[POST]>'
     else:
         return render_template('register.html')
+
+@app.route('/error', methods=['POST', 'GET'])
+def error():
+    return render_template('error.html',message = 'from rout')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
