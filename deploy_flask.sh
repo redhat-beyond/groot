4d20bff20044cef52d8ed5743e71c9df6695f71b
Shay Avraham <shay.avraham100@gmail.com>
2020-05-24 19:00:35 +0300

    Added DB creation

--- deploy_flask.sh
+++ deploy_flask.sh
@@ -8,5 +8,9 @@ apt install -y python3.7 python3-pip postgresql postgresql-contrib
 pip3 install --upgrade pip
 pip3 install -r /vagrant/requirements.txt
 
+# DB creation
+sudo -u postgres createdb groot
+sudo -u postgres psql -c "ALTER ROLE postgres WITH PASSWORD 'groot';"
+
 export FLASK_APP=/vagrant/hello.py
 flask run -h 0.0.0.0 -p $FLASK_PORT >> /vagrant/log.txt 2>&1 &
