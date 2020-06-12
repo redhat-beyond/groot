554ac603e86ed86b82883e1ba66f7f76fd945ed6
Ilan Kushnir <ilan.kushnir@gmail.com>
2020-06-12 16:07:58 +0300

    Showing user policies table

--- groot/models.py
+++ groot/models.py
@@ -9,12 +9,14 @@ def load_user(user_id):
 
 
 policies_users = db.Table('policies_users',
-                       db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-                       db.Column('policy_id', db.Integer, db.ForeignKey('policies.id')))
+                          db.Column('user_id', db.Integer,
+                                    db.ForeignKey('users.id')),
+                          db.Column('policy_id', db.Integer, db.ForeignKey('policies.id')))
 
 sensors_policies = db.Table('sensors_policies',
-                       db.Column('policy_id', db.Integer, db.ForeignKey('policies.id')),
-                       db.Column('sensor_id', db.Integer, db.ForeignKey('sensors.id')))
+                            db.Column('policy_id', db.Integer,
+                                      db.ForeignKey('policies.id')),
+                            db.Column('sensor_id', db.Integer, db.ForeignKey('sensors.id')))
 
 
 class User(db.Model, UserMixin):
@@ -29,15 +31,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(200), nullable=False, unique=False)
     policies_wrote = db.relationship("Policy", backref="owner", lazy=True)
     comments_wrote = db.relationship("Comment", backref="author", lazy=True)
-    policies_used = db.relationship("Policy", secondary=policies_users, 
-                                       backref=db.backref('policies_used', lazy='dynamic'))
-
+    policies_used = db.relationship("Policy", secondary=policies_users,
+                                    backref=db.backref('policies_used', lazy='dynamic'))
 
     def __repr__(self):
-        return {'email':self.email, 'first_name':self.first_name,'last_name':self.last_name,
-         'nick_name':self.nick_name,'password':self.password}
-      
-    
+        return {'email': self.email, 'first_name': self.first_name, 'last_name': self.last_name,
+                'nick_name': self.nick_name, 'password': self.password}
+
 
 class Policy(db.Model):
     __tablename__ = 'policies'
@@ -50,20 +50,19 @@ class Policy(db.Model):
     amount_light = db.Column(db.Integer, nullable=False)
     irregation_frequency = db.Column(db.Integer, nullable=False)
     irregation_amount = db.Column(db.Integer, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+    date_created = db.Column(db.DateTime, nullable=False,
+                             default=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
     writer = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     policy_users = db.relationship("User", backref="used_by", lazy=True)
     policy_comments = db.relationship("Comment", backref="wrote_by", lazy=True)
-    policy_sensors = db.relationship("Sensor", secondary=sensors_policies, 
-                                       backref=db.backref('policy_sensors', lazy='dynamic'))
-
+    policy_sensors = db.relationship("Sensor", secondary=sensors_policies,
+                                     backref=db.backref('policy_sensors', lazy='dynamic'))
 
     def __repr__(self):
-        return {'id':self.id, 'policy_name':self.policy_name, 'plant_type':self.plant_type,
-                'humidity':self.humidity, 'amount_light':self.amount_light,
-                 'irregation_frequency':self.irregation_frequency,
-                 'irregation_amount':self.irregation_amount, 'date_created':self.date_created} 
-
+        return {'id': self.id, 'policy_name': self.policy_name, 'plant_type': self.plant_type,
+                'humidity': self.humidity, 'amount_light': self.amount_light,
+                'irregation_frequency': self.irregation_frequency,
+                'irregation_amount': self.irregation_amount, 'date_created': self.date_created}
 
 
 class Sensor(db.Model):
@@ -75,9 +74,8 @@ class Sensor(db.Model):
     type = db.Column(db.String(30), nullable=False, unique=False)
     is_active = db.Column(db.Boolean, nullable=False, unique=False)
 
-
     def __repr__(self):
-        return {'id':self.id, 'name':self.name,'type':self.type, 'is_active':self.is_active}
+        return {'id': self.id, 'name': self.name, 'type': self.type, 'is_active': self.is_active}
 
 
 class Comment(db.Model):
@@ -87,11 +85,12 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False, unique=False)
     content = db.Column(db.Text, nullable=False, unique=False)
-    date_posted = db.Column(db.String(30), nullable=False, unique=True, default=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
+    date_posted = db.Column(db.String(30), nullable=False, unique=True,
+                            default=datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))
     writer = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    policy_id = db.Column(db.Integer, db.ForeignKey('policies.id'), nullable=False)
-
+    policy_id = db.Column(db.Integer, db.ForeignKey(
+        'policies.id'), nullable=False)
 
 
     def __repr__(self):
-        return {'id':self.id, 'title':self.title,'content':self.content, 'date_posted':self.date_posted}
+        return {'id': self.id, 'title': self.title, 'content': self.content, 'date_posted': self.date_posted}
--- groot/routes.py
+++ groot/routes.py
@@ -4,6 +4,69 @@ from groot.models import *
 from flask_login import login_user, current_user, logout_user, login_required
 import hashlib
 
+user_policies = [
+    {
+        'name': 'Policy 1',
+        'plant_type': 'Mushrooms',
+        'humidity': '0.15',
+        'light': '0.73',
+        'irregation_frequency': '4',
+        'irregation_amount': '0.4',
+        'sensors': '3',
+        'date_created': '03.05.2020',
+        'id': '134623415',
+        'status': 'True'
+    },
+    {
+        'name': 'Policy 1',
+        'plant_type': 'Mushrooms',
+        'humidity': '0.15',
+        'light': '0.73',
+        'irregation_frequency': '4',
+        'irregation_amount': '0.4',
+        'sensors': '3',
+        'date_created': '03.05.2020',
+        'id': '134623415',
+        'status': 'True'
+    },
+    {
+        'name': 'Policy 2',
+        'plant_type': 'Apple Tree',
+        'humidity': '0.24',
+        'light': '0.68',
+        'irregation_frequency': '3',
+        'irregation_amount': '0.5',
+        'sensors': '2',
+        'date_created': '06.07.2020',
+        'id': '123423516',
+        'status': 'False'
+    },
+    {
+        'name': 'Policy 1',
+        'plant_type': 'Mushrooms',
+        'humidity': '0.15',
+        'light': '0.73',
+        'irregation_frequency': '4',
+        'irregation_amount': '0.4',
+        'sensors': '3',
+        'date_created': '03.05.2020',
+        'id': '134623415',
+        'status': 'True'
+    },
+    {
+        'name': 'Policy 2',
+        'plant_type': 'Apple Tree',
+        'humidity': '0.24',
+        'light': '0.68',
+        'irregation_frequency': '3',
+        'irregation_amount': '0.5',
+        'sensors': '2',
+        'date_created': '06.07.2020',
+        'id': '123423516',
+        'status': 'False'
+    }
+]
+
 
 @app.route("/")
 @app.route('/login', methods=['POST', 'GET'])
@@ -18,24 +81,11 @@ def login():
                 login_user(user)
                 next_page = request.args.get('next')
                 return redirect(next_page) if next_page else redirect(url_for('dashboard'))
-            return redirect(url_for('login'))  
+            return redirect(url_for('login'))
         except:
             abort(404)
     else:
         return render_template('login.html')
-
-
-
-def is_user_data_valid(user_data):
-    user = User.query.filter_by(email=user_data['email']).first()
-    if not user:
-        flash('Inccorect email! Please try again.', 'danger')
-        return False
-    if user.password != create_encrypted_password(user_data['password']):
-        flash('Inccorect password! Please try again.', 'danger')
-        return False    
-    return True
-
 
 
 @app.route("/register", methods=['POST', 'GET'])
@@ -45,7 +95,8 @@ def register():
             if current_user.is_authenticated:
                 return redirect(url_for('dashboard'))
             user_data = request.form.to_dict()
-            user_data['password'] = create_encrypted_password(user_data['password'])
+            user_data['password'] = create_encrypted_password(
+                user_data['password'])
             save_user_to_database(user_data)
             flash('Your account has been created! You are now able to log in', 'success')
             return redirect(url_for('login'))
@@ -53,18 +104,6 @@ def register():
             abort(404, description="Unknown error occured")
     else:
         return render_template('register.html')
-
-
-def create_encrypted_password(password):
-    return hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-
-
-def save_user_to_database(user_data):
-    user = User(email=user_data['email'],first_name=user_data['first_name'],
-                last_name=user_data['last_name'],nick_name=user_data['nick_name'],
-                password=user_data['password'])
-    db.session.add(user)
-    db.session.commit()
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
@@ -76,7 +115,7 @@ def dashboard():
 @app.route('/policies', methods=['GET', 'POST'])
 @login_required
 def policies():
-    return render_template('policies.html', title='Policies')
+    return render_template('policies.html', title='Policies', user_policies=user_policies)
 
 
 @app.route('/profile', methods=['GET', 'POST'])
@@ -96,4 +135,26 @@ def sensors():
 def logout():
     logout_user()
     return redirect(url_for('login'))
-	
+
+
+def is_user_data_valid(user_data):
+    user = User.query.filter_by(email=user_data['email']).first()
+    if not user:
+        flash('Inccorect email! Please try again.', 'danger')
+        return False
+    if user.password != create_encrypted_password(user_data['password']):
+        flash('Inccorect password! Please try again.', 'danger')
+        return False
+    return True
+
+
+def create_encrypted_password(password):
+    return hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+
+
+def save_user_to_database(user_data):
+    user = User(email=user_data['email'], first_name=user_data['first_name'],
+                last_name=user_data['last_name'], nick_name=user_data['nick_name'],
+                password=user_data['password'])
+    db.session.add(user)
+    db.session.commit()
