5ab6b25cdcaff89b9467c3b3ef4b0d4c5380ab5c
Ilan Kushnir <ilan.kushnir@gmail.com>
2020-06-14 13:12:31 +0300

    image auto resizing

--- groot/routes.py
+++ groot/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import Flask, Response, render_template, request, redirect, url_for, flash, abort
 from run import app, db
 from groot.models import *
@@ -162,7 +163,13 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
+
     picture_path = os.path.join(
         app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    output_size = (180, 180)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    i.save(picture_path)
     return picture_fn
